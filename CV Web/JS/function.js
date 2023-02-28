e8d80557d9a2c09124d0f9745cbebf18ef7c22bb
franbotteroprogramacion <123417756+franbotteroprogramacion@users.noreply.github.com>
2023-02-28 19:19:01 -0300

    Add files via upload

---
+++ CV Web/JS/function.js
@@ -0,0 +1,23 @@
+function inicio(){
+    location.href = "./../index.html"
+}
+
+function perfil (){
+    location.href = "./../HTML/perfil.html"
+}
+
+function experiencia (){
+    location.href = "./../HTML/expe-laboral.html"
+}
+
+function academica (){
+    location.href = "./../HTML/forma-academica.html"
+}
+
+function personal (){
+    location.href = "./../HTML/info-personal.html"
+}
+
+function linkedin (){
+    location.href = "http://ar.linkedin.com/in/franco-bottero-42239898?trk=people-guest_people_search-card"
+}
